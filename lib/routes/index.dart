f8a214e810620a6b08572423dc13c36b97e31cd8
penglinghuo <qzgcs666@qq.com>
2026-05-27 23:51:32 +0800

    重回正轨

---
+++ lib/routes/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+//返回APP根组件
+Widget getRootWidget() {
+  return MaterialApp(initialRoute: "/", routes: getRootRoutes());
+}
+
+//返回该App的路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {"/": (context) => MainPage(), "login": (context) => LoginPage()};
+}
